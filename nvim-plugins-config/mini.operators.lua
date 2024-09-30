6d2e2412652cadf96cacf8dacd7e2ee4d9bfe881
BrianNixDesktop <briannormant@gmail.com>
2024-09-29 23:47:55 -0400

    swap exchange and sort operator

--- nvim-plugins-config/mini.operators.lua
+++ nvim-plugins-config/mini.operators.lua
@@ -2,15 +2,15 @@
 local legend = {
 	keymaps = {
 		{ "g=", description = "mini-operators evaluate" },
-		{ "sx", description = "mini-operators exchange" },
+		{ "ss", description = "mini-operators exchange" },
 		{ "gm", description = "mini-operators multiply" },
-		{ "gr", description = "mini-operators replace with register" },
-		{ "ss", description = "mini-operators sort text" },
+		{ "gr", mode = {"v"}, description = "mini-operators replace with register" },
+		{ "sx", description = "mini-operators sort text" },
 	},
 }
 _G.LEGEND_append(legend)
 
 require('mini.operators').setup {
-	exchange = { prefix = legend.keymaps[2][1] },
 	sort     = { prefix = legend.keymaps[5][1] },
+	exchange = { prefix = legend.keymaps[2][1] },
 }
