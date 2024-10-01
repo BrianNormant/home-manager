c6cdb66113ed1738cf574ec21ed31d64b53ecc6d
BrianNixDesktop <briannormant@gmail.com>
2024-10-01 03:59:27 -0400

    diagflow disable on certain filetype

--- nvim-plugins-config/diagflow.nvim.lua
+++ nvim-plugins-config/diagflow.nvim.lua
@@ -4,4 +4,13 @@ require('diagflow').setup {
 	toggle_event = { "InsertEnter", "InsertLeave" },
 	render_event = { "CursorMoved", "DiagnosticChanged" },
 	update_event = { "DiagnosticChanged", "BufReadPost" },
+	enable = function()
+		local dis_ft = { "qf", "oil" }
+		for _, ft in ipairs(dis_ft) do
+			if ft == vim.bo.filetype then
+				return false
+			end
+		end
+		return true
+	end
 }
