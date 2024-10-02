59caeb3051340de66405230ac19a9bd6fa8c0fb2
BrianNixLaptop <briannormant@gmail.com>
2024-10-02 15:18:42 -0400

    make documentHighlight check for lsp capabilites

--- nvim-plugins-config/lsp-zero.nvim.lua
+++ nvim-plugins-config/lsp-zero.nvim.lua
@@ -33,7 +33,17 @@ local legend = {
 
 vim.api.nvim_create_autocmd({"CursorHold", "CursorHoldI"}, {
 	pattern = {"*"},
-	callback = function(_) vim.lsp.buf.document_highlight() end
+	callback = function(_)
+		local clients = vim.lsp.get_clients(
+		{ bufnr = vim.api.nvim_get_current_buf() }
+		)
+		for _, c in ipairs(clients) do
+			if c.supports_method('textDocument/documentHighlight') then
+				vim.lsp.buf.document_highlight()
+				break
+			end
+		end
+	end
 })
 vim.api.nvim_create_autocmd({"CursorMoved"}, {
 	pattern = {"*"},
