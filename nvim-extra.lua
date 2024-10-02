716583029d417681a36b44ffea819517aa63d83d
BrianNixDesktop <briannormant@gmail.com>
2024-10-01 22:53:30 -0400

    settings from neovide

--- nvim-extra.lua
+++ nvim-extra.lua
@@ -1,5 +1,17 @@
 if vim.g.neovide then
-	vim.o.font = "FiraCode SemiBold"
+	vim.o.guifont = "FiraCode Nerd Font:h12";
+	vim.g.neovide_floating_shadow = false;
+	vim.g.neovide_position_animation_length = 0.05;
+	vim.g.neovide_scroll_animation_length = 0;
+	vim.g.neovide_scroll_animation_far_lines = 0;
+	vim.g.neovide_hide_mouse_when_typing = true;
+	vim.g.neovide_refresh_rate = 120;
+	vim.g.neovide_cursor_animation_length = 0;
+	if vim.g.neovide then
+		vim.keymap.set({ "n", "v" }, "<C-+>", ":lua vim.g.neovide_scale_factor = vim.g.neovide_scale_factor + 0.1<CR>")
+		vim.keymap.set({ "n", "v" }, "<C-->", ":lua vim.g.neovide_scale_factor = vim.g.neovide_scale_factor - 0.1<CR>")
+		vim.keymap.set({ "n" , "v" }, "<C-0>", ":lua vim.g.neovide_scale_factor = 1<CR>")
+	end
 end
 
 vim.opt.clipboard:append "unnamedplus"
@@ -8,6 +20,7 @@ vim.opt.scrolloff = 5
 vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.laststatus = 3
+vim.opt.cmdheight = 0
 vim.opt.showtabline = 2
 vim.opt.expandtab = false -- set to true to use space instead of tab
 
