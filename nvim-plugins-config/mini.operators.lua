0558e913334aae9b6fb574bfaba2cc1d98811df6
BrianNixLaptop <briannormant@gmail.com>
2024-10-02 13:08:10 -0400

    fix mini bug

--- nvim-plugins-config/mini.operators.lua
+++ nvim-plugins-config/mini.operators.lua
@@ -1,16 +1,17 @@
 --- Keymaps
 local legend = {
 	keymaps = {
-		{ "g=", description = "mini-operators evaluate" },
-		{ "gm", description = "mini-operators multiply" },
-		{ "ss", description = "mini-operators exchange" },
-		{ "sp", description = "mini-operators replace with register" },
-		{ "sx", description = "mini-operators sort text" },
+		[1] = { "g=", description = "mini-operators evaluate" },
+		[2] = { "gm", description = "mini-operators multiply" },
+		[3] = { "ss", description = "mini-operators exchange" },
+		[4] = { "sp", description = "mini-operators replace with register" },
+		[5] = { "sx", description = "mini-operators sort text" },
 	},
 }
 _G.LEGEND_append(legend)
 
 require('mini.operators').setup {
 	sort     = { prefix = legend.keymaps[5][1] },
-	exchange = { prefix = legend.keymaps[2][1] },
+	exchange = { prefix = legend.keymaps[3][1] },
+	replace  = { prefix = legend.keymaps[4][1] },
 }
