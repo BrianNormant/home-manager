8a9b048e017eb24bec0927cbd898c51e3d07f91b
BrianNixLaptop <briannormant@gmail.com>
2024-10-03 12:05:50 -0400

    diagflow disable in quickfix list

--- nvim-plugins-config/diagflow.nvim.lua
+++ nvim-plugins-config/diagflow.nvim.lua
@@ -1,6 +1,8 @@
 require('diagflow').setup {
 	show_borders = true,
-	scope = "line",
+	scope = "cursor",
+	-- i'd prefere line but it shows even when not in the window + it clutter the
+	-- window if there are many diagnostics on the line
 	toggle_event = { "InsertEnter", "InsertLeave" },
 	render_event = { "CursorMoved", "DiagnosticChanged" },
 	update_event = { "DiagnosticChanged", "BufReadPost" },
@@ -8,6 +10,12 @@ require('diagflow').setup {
 		local dis_ft = { "qf", "oil" }
 		for _, ft in ipairs(dis_ft) do
 			if ft == vim.bo.filetype then
+				return false
+			end
+		end
+		local dis_bf = { "quickfix" }
+		for _, bf in ipairs(dis_bf) do
+			if bf == vim.bo.buftype then
 				return false
 			end
 		end
