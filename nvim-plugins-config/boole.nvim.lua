7ab157364a6cd838e4fd572ad4f1143106cbbe83
BrianNixLaptop <briannormant@gmail.com>
2024-10-03 21:16:44 -0400

    Added other items to boole.nvim

--- nvim-plugins-config/boole.nvim.lua
+++ nvim-plugins-config/boole.nvim.lua
@@ -2,5 +2,13 @@ require 'boole'.setup {
 	mappings = {
 		increment = '<C-a>',
 		decrement = '<C-x>',
+	},
+	additions = {
+		{ "red", "green", "blue" },
+		{ "RED", "GREEN", "BLUE" },
+		{ "Red", "Green", "Blue" },
+		{ "black", "white" },
+		{ "BLACK", "WHITE" },
+		{ "Black", "White" },
 	}
 }
