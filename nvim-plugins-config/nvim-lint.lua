53faae4c8175742bfaccc73e62b13fba90ad8da8
BrianNixDesktop <briannormant@gmail.com>
2024-10-05 18:56:16 -0400

    fix checkstyle config and override it's severity to HINT

--- nvim-plugins-config/nvim-lint.lua
+++ nvim-plugins-config/nvim-lint.lua
@@ -1,5 +1,12 @@
 local lint = require('lint')
 
+lint.linters.checkstyle = require("lint.util").wrap(
+	lint.linters.checkstyle, function(diag)
+		diag.severity = vim.diagnostic.severity.HINT
+		return diag
+	end
+)
+
 local checkstyle = require('lint').linters.checkstyle
 checkstyle.args = {"-c", vim.env.HOME .. "/.java/checkstyle/checkstyle.xml"}
 
@@ -7,10 +14,9 @@ lint.linter_by_ft = {
 	java = {"checkstyle"},
 }
 
-
-
 vim.api.nvim_create_autocmd("BufWritePost", {
+	pattern = { "*.java" },
 	callback = function()
-		lint.try_lint()
+		lint.try_lint("checkstyle")
 	end
 })
