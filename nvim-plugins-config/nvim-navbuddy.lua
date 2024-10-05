3704e106357b00f43efbee966d0ef129e6b2dabb
BrianNixDesktop <briannormant@gmail.com>
2024-10-05 01:26:50 -0400

    added a mapping in navbuddy to go the the parent treesiter symbol

--- nvim-plugins-config/nvim-navbuddy.lua
+++ nvim-plugins-config/nvim-navbuddy.lua
@@ -1,3 +1,5 @@
+local actions = require('nvim-navbuddy.actions')
+
 require 'nvim-navbuddy'.setup {
 	window = {
 		border = "single",
@@ -11,14 +13,35 @@ require 'nvim-navbuddy'.setup {
 		},
 	},
 	lsp = { auto_attach = true },
+	mappings = {
+		["<Up>"]    = actions.previous_sibling(),
+		["<Down>"]  = actions.next_sibling(),
+		-- ["<Right>"] = actions.children(),
+		-- ["<Left>"]  = actions.parent(),
+	}
 }
+
+require('nvim-navic').setup {
+	lsp = { auto_attach = true },
+}
+--- https://www.reddit.com/r/neovim/comments/122g5rb/comment/jdup761/
+local goto_parent = function()
+	local symbolPath = require("nvim-navic").get_data()
+	local parent = symbolPath[#symbolPath - 1]
+	if not parent then
+		vim.notify("Already at the highest parent.")
+		return
+	end
+	local parentPos = parent.scope.start
+	vim.api.nvim_win_set_cursor(0, { parentPos.line, parentPos.character })
+end
 
 --- Keymaps
 local legend = {
 	keymaps = {
 		{"<leader>L",  function() require("nvim-navbuddy").open() end, description="Open Navbuddy"},
 		{"L",  require('nvim-navbuddy').open, description="Open Navbuddy"},
+		{"gpp", goto_parent, description = "Jump to symbol parent" },
 	}
 }
 _G.LEGEND_append(legend)
-
