0849d0f531061b8274e0a90fdc012c7fd7a7d6f1
BrianNixDesktop <briannormant@gmail.com>
2025-04-01 22:48:26 -0400

    fix(nixvim): indent blankline colors

--- nixvim/indent-bl.nix
+++ nixvim/indent-bl.nix
@@ -1,29 +1,50 @@
 {pkgs, ... }: {
-	programs.nixvim = {
+	programs.nixvim =
+	let
+		highlight = [
+			"Comment"
+			"RainbowRed"
+			"RainbowYellow"
+			"RainbowBlue"
+			"RainbowOrange"
+			"RainbowGreen"
+			"RainbowViolet"
+			"RainbowCyan"
+		];
+	in {
 		plugins = {
-			rainbow-delimiters.enable = true;
-			indent-blankline.enable = true;
-			indent-blankline.settings = {
-				indent = {
-					highlight = [
-						"Comment"
-						"MarkdownH1"
-						"MarkdownH2"
-						"MarkdownH3"
-						"MarkdownH4"
-						"MarkdownH5"
-						"MarkdownH6"
-					];
-					char = "▏";
-					tab_char = "·";
+			rainbow-delimiters = {
+				inherit highlight;
+				enable = true;
+			};
+			indent-blankline = {
+				lazyLoad.settings = {
+					event = [ "DeferredUIEnter" ];
 				};
-				scope = {
-					enabled = true;
-					highlight = "Blue";
-					char = "║";
-					show_end = false;
+				enable = true;
+				settings = {
+					indent = {
+						inherit highlight;
+						char = "▏";
+						tab_char = "·";
+					};
+					scope = {
+						enabled = true;
+						highlight = "Blue";
+						char = "║";
+						show_end = false;
+					};
 				};
 			};
+		};
+		highlightOverride = {
+			RainbowRed    = { fg = "#E06C75"; };
+			RainbowYellow = { fg = "#E5C07B"; };
+			RainbowBlue   = { fg = "#61AFEF"; };
+			RainbowOrange = { fg = "#D19A66"; };
+			RainbowGreen  = { fg = "#98C379"; };
+			RainbowViolet = { fg = "#C678DD"; };
+			RainbowCyan   = { fg = "#56B6C2"; };
 		};
 	};
 }
