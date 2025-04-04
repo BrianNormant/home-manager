cabd33bccde40d660c65354d09713b86534f7bdf
BrianNixDesktop <briannormant@gmail.com>
2025-04-04 05:35:26 -0400

    fix(nixvim): boole shoudn't be lazy loaded

--- nixvim/boole.lua
+++ nixvim/boole.lua
@@ -1,22 +1,6 @@
-require('lz.n').load {
-	keys = {
-		{"<C-a>", mode = { "v", "n"}},
-		{"<C-x>", mode = { "v", "n"}},
+require 'boole'.setup {
+	mappings = {
+		increment = '<C-a>',
+		decrement = '<C-x>',
 	},
-	after = function ()
-		require 'boole'.setup {
-			mappings = {
-				increment = '<C-a>',
-				decrement = '<C-x>',
-			},
-			additions = {
-				{ "red", "green", "blue" },
-				{ "RED", "GREEN", "BLUE" },
-				{ "Red", "Green", "Blue" },
-				{ "black", "white" },
-				{ "BLACK", "WHITE" },
-				{ "Black", "White" },
-			}
-		}
-	end
 }
