88102ace774be0c3883bbf6a1617c2d7c44a2b4f
BrianNixDesktop <briannormant@gmail.com>
2025-04-15 04:54:44 -0400

    NixVim: Tip on startup

--- nixvim.nix
+++ nixvim.nix
@@ -170,6 +170,16 @@ in {
 				vim.keymap.set({ "n",   "v" },   "<C-S-V>", '"+p', { desc = "Paste system clipboard" })
 				vim.env.NEOVIDE = 1
 			end
+
+			-- Get a cool tip, inspiration from Tip.nvim
+			vim.defer_fn(function()
+				vim.system({"curl", "-L", "https://vtip.43z.one"}, {text = true},
+					function(obj)
+						if (obj.code ~= 0) then return end
+						vim.notify(obj.stdout)
+					end
+				)
+			end, 150)
 		'';
 		autoCmd = [
 			{
