13bef419e70100bf6348518839f1054e1d35dfc8
BrianNixDesktop <briannormant@gmail.com>
2025-04-21 01:30:27 -0400

    Nixvim: ui

--- nixvim/ui.nix
+++ nixvim/ui.nix
@@ -11,6 +11,22 @@ in {
 				plugin = floating-input-nvim;
 				optional = true;
 			}
+			{
+				plugin = buildVimPlugin rec {
+					pname = "ui.nvim";
+					version = "21b0e15";
+					src = fetchFromGitHub {
+						owner = "OXY2DEV";
+						repo = pname;
+						rev = version;
+						hash = "sha256-Tp1bnqnZqptMjD9zedOmamKlrmOd/OS6LJmfPqJ1YPM=";
+					};
+					patches = [
+						./plugin-patch/ui.nvim.patch
+					];
+				};
+				optional = true;
+			}
 		];
 		extraConfigLua = ''
 			require('lz.n').load {
