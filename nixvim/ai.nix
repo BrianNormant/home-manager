cc702f0fcf36ab52a6c79824d74c99bc9fb154d4
BrianNixDesktop <briannormant@gmail.com>
2025-12-17 18:25:48 -0500

    nixvim: use model.nvim

--- nixvim/ai.nix
+++ nixvim/ai.nix
@@ -1,78 +1,64 @@
 {pkgs, config, ... }: {
 	programs.nixvim = {
-		plugins = {
-			# avante = {
-			# 	enable = true;
-			# 	lazyLoad.settings = {
-			# 		enable = false;
-			# 		cmd = "AvanteAsk";
-			# 	};
-			# 	settings = {
-			# 		provider = "ollama";
-			# 		ollama = {
-			# 			model = "gemma3:27b";
-			# 		};
-			# 		vendors = {
-			# 			ollama = {
-			# 				endpoint = "http://ollama.ggkbrian.com";
-			# 			};
-			# 		};
-			# 	};
-			# 	luaConfig.post = ''
-			# 		vim.fn.sign_define("AvanteInputPromptSign", { text = "=>" })
-			# 	'';
-			# };
-		};
 		extraPlugins = [
 			{
 				plugin = pkgs.vimUtils.buildVimPlugin rec {
-					pname = "gen.nvim";
-					version = "e09a8db";
+					pname = "model-nvim";
+					version = "latest";
 					src = pkgs.fetchFromGitHub {
-						owner = "David-Kunz";
-						repo  = "gen.nvim";
-						rev = version;
-						hash = "sha256-s3Ky2uhRviKAaKF3iCF2uHctzk+kFV7BnqyxAGwqhbo=";
+						owner = "gsuuon";
+						repo = "model.nvim";
+						rev = "c4653e9";
+						hash = "sha256-gz97C8/tlU4SDKLaQ5Lv2NbQP8zQRsNxiIQWHoHHDJY=";
 					};
-					patches = [
-						./plugin-patch/gen-nvim.patch
-					];
 				};
 				optional = true;
 			}
 		];
 		extraConfigLua = ''
-			-- we preload ollama by sending a empty request
-			-- vim.system({"zsh", "-c", "ollama run gemma3:27b \"\" &>/dev/null &!"}, {}, function() end)
-			require('lz.n').load { {
-				'gen.nvim',
-				cmd = "Gen",
-				after = function()
-					require('gen').setup {
-						model = "${config.nixvim.ollama_model}",
-						display_mode = 'split',
-						show_prompt = true,
-						show_model = true,
-						no_auto_close = true,
-						host = "ollama.ggkbrian.com",
-						port = 80,
+			require('lz.n').load {
+				"model-nvim",
+				event = "DeferredUIEnter",
+				command = "Model",
+				keys = {
+					{
+						"<S-F1>", "<CMD>Model ask<CR>",
+						mode = {"n", "v"}, desc = "Generate with ollama",
+					},
+				},
+				after = function ()
+					local model = require('model')
+					local ollama = require('model.providers.ollama')
+					local util = require('model.util')
+
+					model.setup {
+						prompts = {
+							["ask"] = {
+								provider = ollama,
+								params = {
+									model = "dolphin3:latest",
+								},
+								builder = function(input)
+
+									local prompt = vim.fn.input("Prompt: ")
+
+									local messages = {
+										{
+											role = "user",
+											content = input,
+										},
+									};
+
+									return {
+										messages = messages,
+										prompt = prompt,
+									}
+								end,
+							},
+						},
 					}
 				end,
-			} }
-		'';
-		keymaps = [
-			{
-				key = "<S-F1>";
-				action = "<CMD>Gen<CR>";
-				options.desc = "Ask Ollama";
-				mode = [ "n" "v" ];
 			}
-			{
-				key = "<F1>";
-				action = "<CMD>AvanteAsk<CR>";
-				options.desc = "Ask Avante";
-				mode = [ "n" "v" ];
-			}
-		];
+			'';
 	};
 }
