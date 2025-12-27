18a5c271f62b68500a7ffbba6c311564e8a2d962
BrianNixLaptop <briannormant@gmail.com>
2025-12-27 18:19:58 -0500

    nxivim: /? in completion toggle fuzzy
    nixvim: /? in completion toggle fuzzy

--- nixvim/completion.lua
+++ nixvim/completion.lua
@@ -1,5 +1,7 @@
 -----------------------[ completeopt and shortmess ]---------------------------
 
+local _M = {}
+
 local snippets = require 'mini.snippets'
 local gen_loader = require('mini.snippets').gen_loader
 snippets.setup {
@@ -43,14 +45,79 @@ completion.setup {
 vim.opt.completeopt = {
 	"menuone",
 	"popup",
-	"fuzzy",
-	"nosort",
-	"noinsert", -- works very well with no insert, allows to search with fuzzy easily
 }
 
 vim.opt.shortmess:append "c"
 vim.o.pumheight = 20
 
+---------------------------------[ Fuzzy on ? ]---------------------------------
+_G.completeswitch = false
+local fuzzy = function(original)
+	-- This function is called when the user presses
+	-- original
+	local mode = vim.fn.complete_info({"mode"}).mode
+	local map = {
+		["keyword"] = "<c-n>",
+		["ctrl_x"] = "",
+		["whole_line"] = "<c-l>",
+		["files"] = "<c-f>",
+		["tags"] = "<c-]>",
+		["path_defines"] = "<c-d>",
+		["path_patterns"] = "<c-i>",
+		["dictionary"] = "<c-k>",
+		["thesaurus"] = "<c-t>",
+		["cmdline"] = "<c-v>",
+		["function"] = "<c-u>",
+		["omni"] = "<c-o>",
+		["spell"] = "<c-s>",
+	}
+	local invalid = {
+		"",
+		"spell",
+		"eval",
+		"unknown",
+		"scroll",
+	};
+	if not vim.tbl_contains(invalid, mode) and not _G.completeswitch then
+		vim.opt.completeopt:append "noselect"
+		vim.opt.completeopt:append "fuzzy"
+		vim.notify("Complete Fuzzy")
+		_G.completeswitch = true
+		return "<c-e>" .. "<c-x>" .. map[mode]
+	else
+		return original
+	end
+end
+
+-- AutoCmd to reset completeopt after completion
+vim.api.nvim_create_autocmd({ "CompleteDone" }, {
+	callback = function()
+		-- vim.notify(vim.inspect(vim.v.event))
+		if not _G.completeswitch then
+			vim.opt_global.completeopt:remove "noselect"
+			vim.opt_global.completeopt:remove "fuzzy"
+		end
+		vim.schedule(function()
+			_G.completeswitch = false
+		end)
+	end,
+})
+
+-- Apply 
+vim.keymap.set(
+	"i",
+	"/",
+	function() return fuzzy("/") end,
+	{ expr = true }
+)
+vim.keymap.set(
+	"i",
+	"?",
+	function() return fuzzy("?") end,
+	{ expr = true }
+)
+
+---------------------------------[ Super-Tab ]----------------------------------
 --- <CR> to select if and only if a item is selected
 --- use autopairs functionnality of:
 --- {|} -> {
