f58ea2c52c54df475fa40b958cf9d6b4c5072cf4
BrianNixDesktop <briannormant@gmail.com>
2026-01-27 16:32:50 -0500

    git: closer to default configs

--- programs/git.nix
+++ programs/git.nix
@@ -18,38 +18,13 @@
 					email = "briannormant@gmail.com";
 					name = hostname;
 				};
-				core = {
-					pager = "delta --pager='ov -F'";
-				};
-				pager = {
-					show = "delta --pager='ov -F --header 3'";
-					diff = "delta --features ov-diff";
-					log  = "delta --features ov-log";
-
-				};
-				delta = {
-					navigate = "true";
-					side-by-side = "true";
-					file-style = "yellow";
-				};
-				"delta \"ov-diff\"" = {
-					pager = "ov -F --section-delimiter '^(commit|added:|removed:|renamed:|Δ)' --section-header --pattern '•'";
-				};
-				"delta \"ov-log\"" = {
-					pager = "ov -F --section-delimiter '^commit' --section-header-num 3";
-				};
 				push = {
 					followTags = "true";
 					default = "upstream"; # Push to the tracked branch (see with git branch -vv)
 				};
-				log.excludeDecoration = "refs/stash";
 				merge.tool = "nvim -c \"Git mergetool\"";
-				"color \"decorate\"" = {
-					HEAD = "blink bold italic 196";
-					branch = "214";
-					tag = "bold 222";
-				};
 				merge.conflictstyle = "diff3";
+				commit.verbose = true;
 			};
 			hooks = {
 				# TODO: pre commit to force commit message to match:
