08b16349af6caf50324090249897b2bc3348271e
BrianNixLaptop <briannormant@gmail.com>
2026-09-13 00:09:17 -0400

    Update nixvim and rmapi

--- extra-packages/rmapi2.nix
+++ extra-packages/rmapi2.nix
@@ -1,9 +1,9 @@
 {pkgs, ...}:pkgs.rmapi.overrideAttrs (old: rec {
-	version = "v0.0.34";
+	version = "v0.0.35";
 	src = pkgs.fetchFromGitHub {
 		owner = "ddvk";
 		repo = "rmapi";
 		rev = "${version}";
-		hash = "sha256-g7KFLa+VBkubzdrgMFDVvAuscw41nyfHd7DWvh3S+NU=";
+		hash = "sha256-mRJH0fQ8e4igR7IwcJdvUhrZDXvpTt/Dac7Pc9p7ITw=";
 	};
 })
